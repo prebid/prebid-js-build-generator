6faff576a26da6b59c9b25a39e32e4859b53a8ae
rgoncharuk <rgoncharuk@rubiconproject.com>
2019-12-11 23:27:42 +0200

    Apply discussed changes

--- buildCleanup.sh
+++ buildCleanup.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
-NUMBER_OF_LATEST_VERSIONS="&1"
+NUMBER_OF_LATEST_VERSIONS="$1"
 
 if [ $NUMBER_OF_LATEST_VERSIONS -gt 0 ]; then
   echo "deleting prebid files older than last $NUMBER_OF_LATEST_VERSIONS versions"
 else
-  NUMBER_OF_LATEST_VERSIONS=1
+  NUMBER_OF_LATEST_VERSIONS=20
   echo "deleting prebid files older than last $NUMBER_OF_LATEST_VERSIONS versions"
 fi
 
 cd prebid.js
 
-LATEST_VERSION=0
-for DIR in prebid_*;
-  do
-    VERSION_SUBSTRING=$(echo $DIR | cut -c8-11)
-    VERSION=$(echo $VERSION_SUBSTRING | tr -d .)
-      if [ $VERSION -gt $LATEST_VERSION ]; then
-        LATEST_VERSION=$VERSION
-      fi
-  done
+VERSION_DIRECTORIES="$(ls -d prebid_* | sort -r)"
 
-LAST_SUPPORTED_VERSION=$(($LATEST_VERSION-$NUMBER_OF_LATEST_VERSIONS+1))
-for DIR in prebid_*;
+ITERATION=0
+for DIR in $VERSION_DIRECTORIES;
   do
-    VERSION_SUBSTRING=$(echo $DIR | cut -c8-11)
-    VERSION=$(echo $VERSION_SUBSTRING | tr -d .)
-      if [ $VERSION -lt $LAST_SUPPORTED_VERSION ]; then
-        echo "deleting directory $DIR"
-        rm -rf $DIR
-      fi
+    if [ $ITERATION -lt $NUMBER_OF_LATEST_VERSIONS ]; then
+      ITERATION=$[ITERATION+1]
+      continue
+    fi
+  echo "deleting directory $DIR"
+  rm -rf $DIR
   done
 
 echo "Versions clean up complete"
