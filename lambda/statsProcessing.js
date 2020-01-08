0cdcd0acc5a4d90c5de0af7b27662280676c6cc3
Sergii Chernysh <schernysh@rubiconproject.com>
2020-01-08 16:53:35 +0200

    Add initial draft of stats processing and reporting feature

---
+++ lambda/statsProcessing.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const zlib = require('zlib');
+const aws = require('aws-sdk');
+
+const { AWS_REGION, DYNAMODB_STATS_TABLE } = process.env;
+
+const dynamoDbRegions = {
+  'us-east-1': true,
+  'us-east-2': true
+};
+
+const dynamoDbClient = new aws.DynamoDB.DocumentClient({
+  apiVersion: '2012-10-08',
+  region: dynamoDbRegions[AWS_REGION] ? AWS_REGION : 'us-east-1'
+});
+
+exports.handler = async (event) => {
+
+    return decompressMessages(event.awslogs.data)
+        .then(payload => {
+            const logMessages = payload.logEvents.map(logEvent => logEvent.message);
+
+            return processMessages(logMessages);
+        });
+};
+
+function decompressMessages(data) {
+    return new Promise((resolve, reject) => {
+        zlib.gunzip(new Buffer(data, 'base64'), (error, buffer) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(buffer);
+            }
+        });
+    })
+    .then(content => {
+       return JSON.parse(content.toString('utf8')) ;
+    });
+}
+
+function processMessages(messages) {
+    const updatePromises = messages.map(message => {
+        const parsedMessage = parseMessage(message);
+        return updateCounters(parsedMessage[0], parsedMessage[1], parsedMessage[2]);
+    });
+
+    return Promise.all(updatePromises);
+}
+
+function parseMessage(message) {
+    const fields = message.split(',');
+    if(fields.length != 5) {
+        console.error('Unexpected log message format: "%s"', message);
+        return;
+    }
+
+    const timestamp = fields[1];
+    const version = fields[2];
+    const modules = fields[3].split(';');
+
+    const hourBucket = new Date(+timestamp).setMinutes(0,0,0);
+
+    return [hourBucket, version, modules];
+}
+
+function updateCounters(hourBucket, version, modules) {
+    const versionPrefix = `version_${version.replace(/\./gi, '_')}`;
+
+    const updateModulesClause = modules
+        .map(module => `${versionPrefix}_count_${module} = if_not_exists(${versionPrefix}_count_${module}, :zero) + :val`)
+        .join(', ');
+
+    return dynamoDbClient.update({
+        TableName: DYNAMODB_STATS_TABLE,
+        Key: {
+            time_bucket: `bucket#${hourBucket}`
+        },
+        UpdateExpression: `set ${versionPrefix}_count = if_not_exists(${versionPrefix}_count, :zero) + :val,${updateModulesClause}`,
+        ExpressionAttributeValues:{
+            ':zero': 0,
+            ':val': 1
+        },
+        ReturnValues: 'NONE'
+    })
+    .promise();
+}
---
+++ lambda/statsReporting.js
@@ -0,0 +1,186 @@
+"use strict";
+
+const aws = require("aws-sdk");
+
+const { AWS_REGION, DYNAMODB_STATS_TABLE } = process.env;
+const { RECIPIENT_ADDRESS_LIST, SOURCE_ADDRESS, REPORT_SUBJECT } = process.env;
+
+const dynamoDbRegions = {
+    "us-east-1": true,
+    "us-east-2": true
+};
+
+const dynamoDbClient = new aws.DynamoDB.DocumentClient({
+  apiVersion: "2012-10-08",
+  region: dynamoDbRegions[AWS_REGION] ? AWS_REGION : "us-east-1"
+});
+
+const sesClient = new aws.SES({region: "us-east-1"});
+
+exports.handler = async (event) => {
+
+    return Promise.all(timeBuckets().map(bucket => fetchBucket(bucket)))
+        .then(fetchedBuckets => aggregateCounts(fetchedBuckets))
+        .then(aggregatedCounts => prepareRawReportData(aggregatedCounts))
+        .then(rawReportData => sendReport(rawReportData));
+};
+
+function timeBuckets() {
+    const lastTimeBucketExclusive = currentMonthStart();
+
+    let buckets = [];
+
+    let currentTimeBucket = previousMonthStart();
+    while(currentTimeBucket < lastTimeBucketExclusive) {
+        buckets.push(`bucket#${currentTimeBucket}`);
+        currentTimeBucket += 3600000; // one hour in milliseconds
+    }
+
+    return buckets;
+}
+
+function currentMonthStart() {
+    // get current time, zero out minutes and hours, set first day of the month 
+    // to get midnight of the first day of current month
+    const date = new Date();
+    // FIXME
+    date.setUTCMonth(11);
+    date.setUTCDate(27);
+
+    date.setUTCMinutes(0,0,0);
+    date.setUTCHours(0);
+    // date.setUTCDate(1);
+
+    return date.getTime();
+}
+
+function previousMonthStart() {
+    // get current month start, move one day back and set first day of the month
+    // const date = new Date(currentMonthStart());
+    // date.setUTCDate(0);
+    // date.setUTCDate(1);
+
+    // FIXME
+    const date = new Date(currentMonthStart());
+    date.setUTCDate(22);
+
+    return date.getTime();
+}
+
+function fetchBucket(bucket) {
+    return dynamoDbClient.get({
+        TableName: DYNAMODB_STATS_TABLE,
+        Key:{
+            time_bucket: bucket
+        }
+    })
+    .promise()
+    .then(data => {
+        return data.Item;
+    });
+}
+
+function aggregateCounts(buckets) {
+    const nonEmptyBuckets = buckets.filter(bucket => bucket != null);
+
+    const aggregatedCounts = {};
+    for (var i in nonEmptyBuckets) {
+        const currentBucket = nonEmptyBuckets[i];
+
+        for(var currentKey in currentBucket) {
+            if(!currentKey.startsWith("version_")) {
+                continue;
+            }
+
+            const previousCount = aggregatedCounts[currentKey] || 0;
+            aggregatedCounts[currentKey] = previousCount + currentBucket[currentKey];
+        }
+    }
+
+    return aggregatedCounts;
+}
+
+function prepareRawReportData(aggregatedCounts) {
+    const versionCounts = {};
+    const latestVersionCounts = {};
+
+    for(var currentKey in aggregatedCounts) {
+        if(currentKey.endsWith("_count")) {
+            // key looks like version_XXX_XXX_XXX_count;
+            // extract version - remove leading version_ and trailing _count from key and
+            // replace underscores with dots
+            const version = currentKey.slice(8).slice(0, -6).replace(/_/gi, ".");
+            versionCounts[version] = aggregatedCounts[currentKey];
+        }
+    }
+
+    const latestVersion = Object.keys(versionCounts).sort().reverse()[0];
+    if(latestVersion) {
+        const latestVersionPrefix = "version_" + latestVersion.replace(/\./gi, "_") + "_count_";
+        for(var currentKey in aggregatedCounts) {
+            if(currentKey.startsWith(latestVersionPrefix)) {
+                const moduleName = currentKey.replace(latestVersionPrefix, "");
+                latestVersionCounts[moduleName] = aggregatedCounts[currentKey];
+            }
+        }
+    }
+
+    return [versionCounts, latestVersionCounts];
+}
+
+function sendReport(rawReportData) {
+    var params = {
+        Destination: {
+            ToAddresses: [RECIPIENT_ADDRESS_LIST]
+        },
+        Message: {
+            Body: {
+                Html: { 
+                    Data: composeReportBody(rawReportData)
+                }
+            },
+            Subject: {
+                Data: REPORT_SUBJECT
+            }
+        },
+        Source: SOURCE_ADDRESS
+    };
+
+
+     return sesClient.sendEmail(params)
+        .promise();
+}
+
+function composeReportBody(rawReportData) {
+    const versionCounts = rawReportData[0];
+    const latestVersionCounts = rawReportData[1];
+
+    const versionsCells = Object.keys(versionCounts).sort().reverse()
+        .map(version => `<tr><td>${version}</td><td>${versionCounts[version]}</td></tr>`)
+        .join("\n");
+
+    const latestVersionModulesCells = Object.keys(latestVersionCounts).sort().reverse()
+        .map(module => `<tr><td>${module}</td><td>${latestVersionCounts[module]}</td></tr>`)
+        .join("\n");
+
+    return `
+        <h1>Monthly report</h1>
+        <h3>Breakdown by version</h3>
+        <table border="1">
+          <tr>
+            <th>Version</th>
+            <th>Number of downloads</th>
+          </tr>
+          ${versionsCells}
+        </table>
+
+        <h3>Breakdown by latest version modules</h3>
+        <b>Version: 2.30.0</b>
+        <table border="1">
+          <tr>
+            <th>Module</th>
+            <th>Number of downloads</th>
+          </tr>
+          ${latestVersionModulesCells}
+        </table>`;
+}
